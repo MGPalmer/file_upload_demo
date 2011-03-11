0c0bbb386f97a783b5e92707a16fceab1121ec63
Martin Tepper <mgpalmer@monogreen.de>
2011-03-11 22:00:48 +0100

    * Adding JS file-uploader script * Allowing two paths in controller to make ajax upload working (not all params can be changed so it'd just work with AR) * Retaining non-JS upload

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -9,7 +9,31 @@ class UploadsController < ApplicationController
   end
 
   def create
-    @upload = Upload.new(params[:upload])
+    if request.xhr?
+      ajax_upload(request, params)
+    else
+      proper_upload(params)
+    end
+  end
+
+  private
+
+  def ajax_upload(request, params)
+    @upload = Upload.new(
+      :original_filename => params['qqfile'],
+      :uuid => params['X-Progress-ID'],
+      :data => request.body
+      # TODO: comment
+     )
+    if @upload.save
+      render :json => {:success => true}
+    else
+      #TODO
+    end
+  end
+
+  def proper_upload(params)
+    @upload = Upload.new(params[:upload].merge(:uuid => params['X-Progress-ID']))
     if @upload.save
       redirect_to :action => "show", :id => @upload.id
     else
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,8 +3,11 @@ class Upload < ActiveRecord::Base
   attr_accessor :data
 
   validates_presence_of :uuid
+  validates_presence_of :data, :message => "Please choose a file to upload."
 
+  before_save :save_original_filename
   after_save :save_to_disk
+
   def initialize(*args)
     super
     self.uuid ||= UUID.generate(:compact)
@@ -14,10 +17,18 @@ class Upload < ActiveRecord::Base
     File.join(Rails.root, "public", "uploads", uuid)
   end
 
+  def size
+    File.size?(path)
+  end
+
   def save_to_disk
     File.open(path, "wb") do |f|
       f.write(self.data.read)
     end
+  end
+
+  def save_original_filename
+    self.original_filename ||= self.data.original_filename
   end
 
 end
