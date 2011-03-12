18f1b1052cea25093ffe7a3f25e7e2a10baa7c95
Martin Tepper <mgpalmer@monogreen.de>
2011-03-12 18:04:04 +0100

    * Adding specs - mostly pendings for now * Generating rspec boilerplate files

--- spec/controllers/uploads_controller_spec.rb
+++ spec/controllers/uploads_controller_spec.rb
@@ -7,4 +7,13 @@ describe UploadsController do
     controller.should be_an_instance_of(UploadsController)
   end
 
+  it "finds the upload for show"
+  it "gets a new upload for new"
+  it "updates the title of an upload and displays json data with update"
+  it "munges parameters into model form on create with xhr"
+  it "renders json on create with xhr"
+  it "adds the uuid from parameters into model params on create without xhr"
+  it "redirects to show after successful save on create without xhr"
+  it "shows the form with errors again on create without xhr"
+
 end
--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -3,13 +3,23 @@ require 'spec_helper'
 describe Upload do
   before(:each) do
     @valid_attributes = {
-      :uuid => "value for uuid",
-      :path => "value for path",
-      :comment => "value for comment"
+      :uuid => "schnippschnappschnupp",
+      :original_filename => "Motörhead electro dance remix IXX.mp3",
+      :title => "best track ever !"
     }
   end
 
   it "should create a new instance given valid attributes" do
     Upload.create!(@valid_attributes)
   end
+
+  it "'s not valid without an uuid"
+  it "'s not valid without data"
+  it "generates the path from the uuid"
+  it "generates a default uuid on initialize"
+  it "uses a given uuid"
+  it "gives out a default title if none is present"
+  it "gets the size from the filesystem"
+  it "saves data to disk after create"
+  it "sets the original filename before and without saving"
 end
